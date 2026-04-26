53d3361cc4957d7df0778c5b14be956cd1184281
Swapnil <swapnil@gmail.com>
2026-04-26 06:49:08 +0000

    requriement name change

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
 
   tags = {
-            Name = "New test Instance test q"
+            Name = "New test Instance test requirement check"
   }
 }
